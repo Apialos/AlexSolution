9df781ab31784ac1618d87b204828aad8b1be43c
Alex <al.pialoglou@gmail.com>
2017-05-21 18:27:34 +0300

    start of my new project

---
+++ Business/FirstHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Business
+{
+    public class FirstHelper
+    {
+        public int MyProperty { get; set; }
+
+        private static readonly string _Fg = "gfdj";
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ WebApp1/wwwroot/js/programm.js
@@ -0,0 +1,15 @@
+﻿// Place third party dependencies in the lib folder
+// Configure loading modules from the lib directory,
+// except 'app' ones, 
+requirejs.config({
+    //"baseUrl": "js",
+    paths: {
+        "jquery": "lib/jquery",
+    }
+});
+
+// Start the main app logic.
+requirejs([
+    "lib/jquery",
+    "modules/main"
+]);
